c15825ec65492ca86b6c5ddfb4295b31c7a595b7
samanthamarberger <slatham04@gmail.com>
2022-09-22 13:50:04 -0600

    commit tree 99415cc701fcad91d240faea68343bdaea46e5f8

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function  destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function  destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newAppendCats = [...cats, name];
+    return newAppendCats;
+}
+
+function prependCat(name) {
+    const newPrependCats = [name, ...cats];
+    return newPrependCats;
+}
+
+function removeLastCat() {
+    const newRemoveLastCat = [...cats];
+    newRemoveLastCat.pop();
+    return newRemoveLastCat;
+}
+
+function removeFirstCat() {
+    const newRemoveFirstCat = [...cats];
+    newRemoveFirstCat.shift();
+    return newRemoveFirstCat;
+}
